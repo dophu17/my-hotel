b6e699f428d0d6a1ea8861b1808a08436523709b
DoPhu <dophu17@gmail.com>
2018-12-04 22:10:21 +0700

    init

---
+++ resources/views/admins/homes/home.blade.php
@@ -0,0 +1,5 @@
+@extends('admins.app')
+
+@section('content')
+123
+@endsection
---
+++ resources/views/admins/layouts/script.blade.php
@@ -0,0 +1,10 @@
+<!--begin::Base Scripts -->
+<script src="{{ asset('') }}metronic/assets/vendors/base/vendors.bundle.js" type="text/javascript"></script>
+<script src="{{ asset('') }}metronic/assets/demo/default/base/scripts.bundle.js" type="text/javascript"></script>
+<!--end::Base Scripts -->   
+<!--begin::Page Vendors -->
+<script src="{{ asset('') }}metronic/assets/vendors/custom/fullcalendar/fullcalendar.bundle.js" type="text/javascript"></script>
+<!--end::Page Vendors -->  
+<!--begin::Page Resources -->
+<script src="{{ asset('') }}metronic/assets/demo/default/custom/components/calendar/basic.js" type="text/javascript"></script>
+<!--end::Page Resources -->
